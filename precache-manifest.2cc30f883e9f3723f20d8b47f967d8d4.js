5d3a16826032abb4f7b54565be848e2a57906b64
Tiffany <tiffany830326@gmail.com>
2019-08-23 23:47:34 +0800

    Updates

---
+++ precache-manifest.2cc30f883e9f3723f20d8b47f967d8d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12452ca41055bc3c3f5d84bb584a6388",
+    "url": "/hotel-reservation/index.html"
+  },
+  {
+    "revision": "e17b28f8c876d2410c42",
+    "url": "/hotel-reservation/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "1188a89a8165930960a2",
+    "url": "/hotel-reservation/static/css/main.61a8eb13.chunk.css"
+  },
+  {
+    "revision": "e17b28f8c876d2410c42",
+    "url": "/hotel-reservation/static/js/2.2df75220.chunk.js"
+  },
+  {
+    "revision": "1188a89a8165930960a2",
+    "url": "/hotel-reservation/static/js/main.e8aab00f.chunk.js"
+  },
+  {
+    "revision": "e0c50a0689d5510c612f",
+    "url": "/hotel-reservation/static/js/runtime~main.de75002c.js"
+  }
+]);
